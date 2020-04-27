dc318b16d6fc49f28c12e26966bb72c9ed7a8f89
karravularajeswari <43335512+karravularajeswari@users.noreply.github.com>
2020-04-27 14:28:19 +0530

    Add files via upload

---
+++ TempDataSessionExController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class TempDataSessionExController : Controller
+    {
+        // GET: TempDataSessionEx
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string txtname,string txtprice)
+        {
+            TempData["uname"] = txtname;
+            Session["Name"] = txtprice;
+            return RedirectToAction("Details");
+        }
+        public ViewResult Details()
+        {
+            return View();
+        }
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+    }
+}
